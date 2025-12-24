dd51eace103872f316a9a6c7b14c3d7577f6934a
quhou <yingxiang.cao@autobizline.com>
2025-12-24 12:13:17 +0000

    refactor: python django to node express

--- theme/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.wavespeed_feature, name='home'),
-    path('feature/<str:keyword>', views.wavespeed_feature, name='feature'),
-]
